095466869e4bcb90d9594af21df9787abe1993e0
Justing24 <ginesej1@newpaltz.edu>
2022-10-23 20:59:18 -0400

    Update Nav.vue

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -14,7 +14,7 @@ let isOpen = ref(false);
   <nav class="navbar is-info" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
       <a class="navbar-item" href="https://bulma.io">
-        <img src="logo.jpg" width="112" height="28">
+        <img src="../assets/logo.jpg" width="112" height="28">
       </a>
 
       <a role="button" :class="{ 'is-active': isOpen }" @click="isOpen = !isOpen"  class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
