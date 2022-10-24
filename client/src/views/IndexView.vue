46661da0d14210e14c1f61795506187421d9103c
Justing24 <ginesej1@newpaltz.edu>
2022-10-23 20:57:58 -0400

    Update IndexView.vue

--- client/src/views/IndexView.vue
+++ client/src/views/IndexView.vue
@@ -1,14 +1,7 @@
 <template>
     <div>
-        <section class="hero is-large is-info">
+        <section class="hero is-large">
             <div class="hero-body">
-                <p class="title">
-                    Index Landing page
-                </p>
-                <p class="subtitle">
-                    This is a landing page
-
-                </p>
                 <p> Please pick a user from the dropdown to sign in.
                 </p>
               
